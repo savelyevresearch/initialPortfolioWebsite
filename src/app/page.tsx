9f82273bbde42a64625c6258ce53ef12d3112874
savelyevresearch <savelyevresearch@protonmail.com>
2023-08-28 20:37:46 +0600

    Components for the root page have been inserted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,29 @@
-import Image from "next/image";
+import Navbar from "@/components/Navbar/Navbar";
+import Hero from "@/components/Hero/Hero";
+import About from "@/components/About/About";
+import Exprerience from "@/components/Experience/Experience";
+import Tech from "@/components/Tech/Tech";
+import Works from "@/components/Works/Works";
+import Feedbacks from "@/components/Feedbacks/Feedbacks";
+import Contact from "@/components/Contact/Contact";
+import StarsCanvas from "@/components/StarsCanvas/StarsCanvas";
 
 export default function Home() {
-  return <div>Web Developer and Cybersecurity Engineer porfolio</div>;
+  return (
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat">
+        <Navbar />
+        <Hero />
+      </div>
+      <About />
+      <Exprerience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className="relative z-0">
+        <Contact />
+        <StarsCanvas />
+      </div>
+    </div>
+  );
 }
